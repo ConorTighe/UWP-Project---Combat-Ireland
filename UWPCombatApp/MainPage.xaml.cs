48f3a02e6162e30d315d60451ad3fec1c8c19dfe
Conor <conor.tighe22@gmail.com>
2017-10-17 15:52:31 +0100

    Menu Template

---
+++ UWPCombatApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPCombatApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<AdaptItem> picItems_;
+
+        private ObservableCollection<AdaptItem> PicItems
+        {
+            get
+            {
+                return picItems_;
+            }
+            set
+            {
+                picItems_ = value;
+            }
+
+        }
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            picItems_ = AdaptItem.AdaptList();
+            AGVC.ItemsSource = picItems_;
+            
+
+
+
+        }
+
+
+    }
+}
---
+++ UWPCombatApp/MenuTemplate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPCombatApp
+{
+    public class AdaptItem
+    {
+        public String Image
+        {
+            get;
+            set;
+        }
+
+        public static ObservableCollection<AdaptItem> AdaptList()
+        {
+            ObservableCollection<AdaptItem> pics = new ObservableCollection<AdaptItem>()
+        {
+            new AdaptItem
+            {
+                Image = "ms-appx:///Assets/b1.png"
+            },
+            new AdaptItem
+            {
+                Image = "ms-appx:///Assets/b2.png"
+            },
+            new AdaptItem
+            {
+                Image = "ms-appx:///Assets/b3.png"
+            },
+            new AdaptItem
+            {
+                Image = "ms-appx:///Assets/b4.png"
+            },
+            new AdaptItem
+            {
+                Image = "ms-appx:///Assets/b5.png"
+            }
+        };
+
+            return pics;
+        }
+    }
+}
